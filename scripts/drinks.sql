b5d8ee4b7f3d3d0a5d4a0e619ad74386de72227d
Andrew Omelchenko <andrewom43@gmail.com>
2018-12-08 17:11:27 +0200

    add menu data

--- scripts/drinks.sql
+++ scripts/drinks.sql
@@ -1,7 +1,6 @@
 CREATE TABLE drinks (
 	id SERIAL PRIMARY KEY,
 	item VARCHAR(100),
-	category VARCHAR(100),
-	subcategory VARCHAR(100),
+	category VARCHAR(50),
 	price NUMERIC(8, 2) DEFAULT 0.00
 );
--- scripts/menu.sql
+++ scripts/menu.sql
@@ -2,8 +2,6 @@ CREATE TABLE menu (
 	id SERIAL PRIMARY KEY,
 	item VARCHAR(100),
 	description TEXT,
-	category VARCHAR(100),
-	subcategory VARCHAR(100),
-	image VARCHAR(100),
+	category VARCHAR(50),
 	price NUMERIC(8, 2) DEFAULT 0.00
 );
