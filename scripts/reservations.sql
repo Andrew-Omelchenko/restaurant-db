72b1421e626684d1cfbc889803cc1b37bf036786
Andrew Omelchenko <andrewom43@gmail.com>
2018-12-11 21:48:32 +0200

    add script for reservations table creation

---
+++ scripts/reservations.sql
@@ -0,0 +1,9 @@
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY,
+  client_email TEXT NOT NULL,
+  table_id integer NOT NULL,
+  reserved_date DATE NOT NULL,
+  reserved_hour integer NOT NULL,
+  notes TEXT,
+  CONSTRAINT order_constraint UNIQUE (table_id, reserved_date, reserved_hour)
+);
